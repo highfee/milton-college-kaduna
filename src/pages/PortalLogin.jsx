fa82ffa3fe7eac69a9f619cde127feabd023a827
missioncomputertrainingcentre <missioncomputertrainingcentre@gmail.com>
2026-02-20 06:58:39 +0000

    File changes

--- src/pages/PortalLogin.jsx
+++ src/pages/PortalLogin.jsx
@@ -100,7 +100,7 @@ export default function PortalLogin() {
           <p className="text-gray-600">Choose your portal to access the system</p>
         </div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
           {portals.map((portal) => {
             const Icon = portal.icon;
             return (
