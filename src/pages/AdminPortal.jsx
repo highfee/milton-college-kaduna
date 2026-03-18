0a5da10f0a20e412fb620dccaecb4c62c9466fe2
MISSION COMPUTER TRAINING CENTRE <missioncomputertrainingcentre@gmail.com>
2026-03-18 12:06:48 +0000

    File changes

--- src/pages/AdminPortal.jsx
+++ src/pages/AdminPortal.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// AdminPortal
 import { base44 } from '@/api/base44Client';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '../utils';
