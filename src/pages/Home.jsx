7dc7d62259909a14bcdf9a2f6ae841ddf710570e
MISSION COMPUTER TRAINING CENTRE <missioncomputertrainingcentre@gmail.com>
2026-03-21 12:41:25 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -473,7 +473,7 @@ export default function Home() {
 
       {/* Floating WhatsApp Button */}
       <a 
-        href="https://wa.me/2349067879088" 
+        href="https://wa.me/2348033492870" 
         target="_blank" 
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 w-14 h-14 bg-green-500 rounded-full flex items-center justify-center shadow-lg hover:bg-green-600 transition-colors z-50"
