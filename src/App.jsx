f143a6e43074b12af72651503ecf5d0988520132
MISSION COMPUTER TRAINING CENTRE <missioncomputertrainingcentre@gmail.com>
2026-06-02 11:06:28 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import MarkAttendance from './pages/MarkAttendance';
+import StudentAssignments from './pages/StudentAssignments';
+import StudentAssignmentScores from './pages/StudentAssignmentScores';
+import StudentLMS from './pages/StudentLMS';
+import TeacherCMS from './pages/TeacherCMS';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
@@ -58,6 +63,11 @@ const AuthenticatedApp = () => {
           }
         />
       ))}
+      <Route path="/MarkAttendance" element={<LayoutWrapper currentPageName="MarkAttendance"><MarkAttendance /></LayoutWrapper>} />
+      <Route path="/StudentAssignments" element={<LayoutWrapper currentPageName="StudentAssignments"><StudentAssignments /></LayoutWrapper>} />
+      <Route path="/StudentAssignmentScores" element={<LayoutWrapper currentPageName="StudentAssignmentScores"><StudentAssignmentScores /></LayoutWrapper>} />
+      <Route path="/StudentLMS" element={<LayoutWrapper currentPageName="StudentLMS"><StudentLMS /></LayoutWrapper>} />
+      <Route path="/TeacherCMS" element={<LayoutWrapper currentPageName="TeacherCMS"><TeacherCMS /></LayoutWrapper>} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
